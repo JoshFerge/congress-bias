336a616389a7e3771ceed443fb7268b15ba920ec
Josh Ferge <josh.ferge@gmail.com>
2015-12-29 06:32:18 +0000

    coming together

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,70 @@
+
+var PartyGuesserApp = angular.module('PartyGuesserApp', []);
+
+
+
+PartyGuesserApp.controller('mainCtrl', function ($rootScope,$scope,$http) {
+
+  $scope.currentIndex = 0
+
+  $scope.currentSession = {
+    correct: [],
+    incorrect: []
+  };
+
+
+  $scope.getInfo = function() {
+    $http.get("/senators").success(function(response) {
+      $scope.senatorInfo = $scope.shuffle(response);
+
+    });
+  };
+  $scope.getInfo();
+
+  $scope.advance = function(party) {
+
+    var currentSenator = $scope.senatorInfo[$scope.currentIndex]
+    if (party === currentSenator.party) {
+      $scope.currentSession.correct.push(currentSenator);
+    }
+    else {
+      $scope.currentSession.incorrect.push(currentSenator);
+    }
+
+    $http.post("/senators/"+currentSenator.name, {'senator':currentSenator});
+
+    if ($scope.currentIndex < $scope.senatorInfo.length-1) {
+      $scope.currentIndex+=1;
+    }
+    else {
+      alert('you done');
+    }
+
+    console.log($scope.currentSession);
+  }
+
+
+
+
+  $scope.shuffle = function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
+});
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/congress-bias');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('working');
+});
+
+
+var senatorSchema = mongoose.Schema({
+    name: String,
+    state: String,
+    party: String,
+    averageAccuracy: Number,
+    guessesRight: Number,
+    guessesWrong: Number,
+    image: String
+});
+
+
+
+var Senator = mongoose.model('Senator', senatorSchema);
+
+// var g = {"senatorInfo":[{'name':'Bernie Sanders','state':'Vermont','party':'Democrat','image':'/images/bernie_sanders.jpg'}]};
+
+// Senator.collection.insertMany(g.senatorInfo, function(err,r) {
+//     console.log(err);
+//     console.log(r);
+//     db.close();
+// });
+
+router.get('/senators', function(req, res) {
+    Senator.find({}, function(err, users) {
+        res.send(users);
+    });
+});
+router.get('/senators/:senator', function(req, res) {
+    Senator.find({'name': req.params.senator}, function(err, senator) {
+        res.send(senator);
+    });
+});
+
+router.post('/senators/:senator', function(req, res) {
+    console.log(req.body.senator.name);
+    Senator.findOneAndUpdate({ 'name': req.body.senator.name }, { $inc: { 'guessesRight': 1 }}, null, function(err, res) {console.log(err,res);});
+    res.send('OK');
+});
+
+router.get('/test', function(req, res) {
+    Senator.find({'name': req.params.senator}, function(err, senator) {
+        res.render('test');
+    });
+});
+
+router.post('/results', function(req, res) {
+    res.render('results');
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
