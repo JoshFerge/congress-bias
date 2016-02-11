6860000e59fc4528039df595fcf72f2899d51eb9
Josh Ferge <josh.ferge@gmail.com>
2016-02-11 20:04:32 +0000

    added done button and mobile enhancements

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -8,29 +8,18 @@ window.onload = function () {
   l.addEventListener('animationend', function() {
     document.getElementById('indicator').className = "";
   });
-
-   r.addEventListener('animationend', function() {
+  r.addEventListener('animationend', function() {
     document.getElementById('rightindicator').className = "";
   });
-
-
 };
 
-
-
-
-
 PartyGuesserApp.controller('mainCtrl', function ($rootScope,$scope,$http) {
-
-
-
-  $scope.currentIndex = 0
+  $scope.currentIndex = 0;
 
   $scope.currentSession = {
     correct: [],
     incorrect: []
   };
-
 
   $scope.getInfo = function() {
     $http.get("/senators").success(function(response) {
@@ -41,40 +30,35 @@ PartyGuesserApp.controller('mainCtrl', function ($rootScope,$scope,$http) {
   $scope.getInfo();
 
   $scope.advance = function(party) {
+    var currentSenator = $scope.senatorInfo[$scope.currentIndex];
 
-    var currentSenator = $scope.senatorInfo[$scope.currentIndex]
-
-    console.log(party)
-    console.log(currentSenator.party)
     if (party === currentSenator.party) {
       $scope.currentSession.correct.push(currentSenator);
 
       if (party === 'Republican') {
-        r.innerHTML = 'correct'
-        r.className = 'animateit-right'
-        r.style.color = 'green'
+        r.innerHTML = 'correct';
+        r.className = 'animateit-right';
+        r.style.color = 'green';
       }
       else {
-        l.className = 'animateit'
-        l.innerHTML = 'correct'
-        l.style.right = '0'
-        l.style.color = 'green'
+        l.className = 'animateit';
+        l.innerHTML = 'correct';
+        l.style.right = '0';
+        l.style.color = 'green';
       }
     }
     else {
       $scope.currentSession.incorrect.push(currentSenator);
       if (party === 'Republican') {
-        r.innerHTML = 'incorrect'
-        r.className = 'animateit-right'
-        r.style.color = 'red'
-
+        r.innerHTML = 'incorrect';
+        r.className = 'animateit-right';
+        r.style.color = 'red';
       }
       else {
-        l.innerHTML = 'incorrect'
-        l.className = 'animateit'
-        l.style.right = '0'
-        l.style.color = 'red'
-
+        l.innerHTML = 'incorrect';
+        l.className = 'animateit';
+        l.style.right = '0';
+        l.style.color = 'red';
       }
     }
 
@@ -82,36 +66,31 @@ PartyGuesserApp.controller('mainCtrl', function ($rootScope,$scope,$http) {
 
     if ($scope.currentIndex < $scope.senatorInfo.length-1) {
       $scope.currentIndex+=1;
+      console.log($scope.currentIndex);
+      if ($scope.currentIndex > 9) {
+        console.log('hi')
+        document.getElementById('Done').style.visibility = "visible";
+      }
     }
     else {
       alert('you done');
     }
-
-    console.log($scope.currentSession);
   }
 
-
-
-
-  $scope.shuffle = function(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-};
-
+    $scope.shuffle = function(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    };
 });
 
 
